ef40a8f1059bcc72db2c96ccabb6e900350e1962
Klint <klintwales@gmail.com>
2021-04-11 12:43:11 -0500

    clean up

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App.jsx';
 
 
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
