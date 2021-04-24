f0c142d10c08cdf4c854f27a6311f860f02f41a4
Klint <klintwales@gmail.com>
2021-04-23 19:22:51 -0500

    update comment

--- src/components/homePage.jsx
+++ src/components/homePage.jsx
@@ -5,7 +5,7 @@ import { Player } from 'video-react';
 function HomePage(){
   return(
     <div>
-    /*had to add the ratio and ratio-16x9 in the enclosing div in order for the video to render correctly */
+    /*had to add the ratio and ratio-16x9 (instead of embed-responsive-16by9 noted in the bootstrap documentation) in the enclosing div in order for the video to render correctly */
       <div class="embed-responsive ratio ratio-16x9">
         <iframe class="video embed-responsive-item w-100" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen></iframe>
       </div>
