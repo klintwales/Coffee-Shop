f1f64fde7f6c6a0bb442c99d914ffd905b67beb1
Klint <klintwales@gmail.com>
2021-04-25 15:10:30 -0500

    add about blurb and import to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './components/header.jsx'
 import HomePage from './components/homePage.jsx'
+import AboutBlurb from './components/aboutBlurb.jsx'
 
 function App() {
   return (
     <div>
     <Header />
     <HomePage />
+    <AboutBlurb />
     </div>
   );
 }
--- src/components/homePage.jsx
+++ src/components/homePage.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import { Player } from 'video-react';
+import coffee from "../assets/coffee.jpg"
+
 
 
 function HomePage(){
   return(
     <div>
-    /*had to add the ratio and ratio-16x9 (instead of embed-responsive-16by9 noted in the bootstrap documentation) in the enclosing div in order for the video to render correctly */
-      <div class="embed-responsive ratio ratio-16x9">
-        <iframe class="video embed-responsive-item w-100" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen></iframe>
+    {/*had to add the ratio and ratio-16x9 (instead of embed-responsive-16by9 noted in the bootstrap documentation) in the enclosing div in order for the video to render correctly */}
+      <div className="embed-responsive ratio ratio-16x9">
+        <img className="embed-responsive-item w-100" src={coffee} alt="coffee"></img>
       </div>
     </div>
   )
