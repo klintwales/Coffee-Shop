15798412115880f6df01d62fcdf6d6d0c12300c7
Klint <klintwales@gmail.com>
2021-06-08 22:39:07 -0500

    Add place holder menu page. Add router in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
+import React from "react";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Header from './components/header.jsx'
 import HomePage from './components/homePage.jsx'
-import AboutBlurb from './components/aboutBlurb.jsx'
+
 import Footer from './components/footer.jsx'
+import Menu from './components/menu.jsx'
 
 function App() {
   return (
     <div>
+    <Router>
     <Header />
-    <HomePage />
-    <AboutBlurb />
+    <Switch>
+    <Route path="/" exact component={() => <HomePage />}/>
+    <Route path="/menu" exact component={() => <Menu />}/>
+    </Switch>
     <Footer />
+    </Router>
     </div>
   );
 }
