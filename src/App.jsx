2bb0ed91e22ea6270e11224749b7dd6964058903
Klint <klintwales@gmail.com>
2021-07-07 20:48:12 -0500

    Add cart component, hook up routing, and link for button

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Header from './components/header.jsx'
 import HomePage from './components/homePage.jsx'
+import Cart from './components/cart.jsx'
 
 import Footer from './components/footer.jsx'
 import Menu from './components/menu.jsx'
@@ -14,6 +15,7 @@ function App() {
     <Switch>
     <Route path="/" exact component={() => <HomePage />}/>
     <Route path="/menu" exact component={() => <Menu />}/>
+    <Route path="/cart" exact component={() => <Cart />}/>
     </Switch>
     <Footer />
     </Router>
---
+++ src/components/cart.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+
+
+function Cart(){
+  return(
+    <div>
+      <h1>Shopping Cart</h1>
+        <div>
+          <br/>
+          <br/>
+          <h2>Items</h2>
+            <div>
+            <div class="bg-dark me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+            <div class="my-3 py-3">
+            <h2 class="display-5">Placeholder</h2>
+            <p class="lead">This is where the menu item list will go</p>
+            </div>
+            <div class="bg-light shadow-sm mx-auto" style={{width:"50%", height:"300px", borderRadius:"21px 21px 0 0"}}></div>
+            </div>
+            </div>
+        </div>
+    </div>
+
+  );
+}
+
+export default Cart;
