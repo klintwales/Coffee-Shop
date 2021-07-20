e7ea72ca6950969997434d48824cac3ffbeac09a
Klint <klintwales@gmail.com>
2021-07-20 18:21:57 -0500

    Finalize removing items form cart

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -6,13 +6,34 @@ function Cart(){
 
 const [items, removeItem] = useState(window.shoppingCart)
 
-function deleteItem(id){
-  console.log(items);
-  items.splice(id);
-  removeItem(items);
+//Called by the cartItem onClick
+function deleteItem(props){
+  console.log(props.id);
+  //React requires a funtion to be passed for using state
+  removeItem(removeItemFromArray(props));
+}
+
+//Iterates through window.shoppingCart and adds all of the items except for the item that was deleted to a temporary array, then sets window.shopping equal to the new temp array
+function removeItemFromArray(props){
+  console.log("props");
+  console.log(props);
+  let newArray = [];
+    for(let i = 0; i < window.shoppingCart.length; i++){
+      if (i != props.id){
+        newArray.push(window.shoppingCart[i]);
+        console.log(window.shoppingCart[i]);
+      }
+    }
+  console.log(newArray);
+  window.shoppingCart = newArray;
+  return (newArray);
+
 }
 
 
+
+
+//Creates cart item for each item in window.shoppingCart
   return(
     <div>
       <h1 style={{marginTop:"1%"}}>Shopping Cart</h1>
@@ -22,13 +43,13 @@ function deleteItem(id){
           <h2>Items</h2>
           <br/>
           <div>
-            {items.map((item) => {
+            {items.map((newItem, index) => {
               return(
                 <CartItem
-                key={item.index}
-                id={item.index}
-                itemName={item.item}
-                itemPrice={item.price}
+                key={index}
+                id={index}
+                itemName={newItem.item}
+                itemPrice={newItem.price}
                 onDelete={deleteItem}
                 />
               )
